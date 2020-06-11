9525a5bce3b2b29a9b60d382cc87efd181e0f610
tigerlittle1 <44134547+tigerlittle1@users.noreply.github.com>
2020-06-12 01:13:26 +0800

    新增調整光線酸價功能

--- Seneor_Client.py
+++ Seneor_Client.py
@@ -35,16 +35,23 @@ class Sensor_Light_Layout(BoxLayout):
         self.lium_label = Label(text = "")
         self.add_widget(self.lium_label)
 
-        self.close_button = Button(text="Close Connect",size_hint = (1,0.25))
+        self.seting_button = Button(text="Set cfg",size_hint = (1,0.25))#設定按鈕
+        self.seting_button.bind(on_press=self.setting)
+        self.add_widget(self.seting_button)
 
+        self.close_button = Button(text="Close Connect",size_hint = (1,0.25))#關閉按鈕
         self.close_button.bind(on_press=self.close)
         self.add_widget(self.close_button)
+
+
 
 
         self.internet = Client.Client()
         self.illumination = 0
 
         self.data = []
+        self.Acid1 = 0
+        self.Acid2 = 0
 
     def listen_sever(self):
         try:
@@ -56,10 +63,10 @@ class Sensor_Light_Layout(BoxLayout):
 
         while True:
             try:
-
                 self.internet.get_message()#等待要求
                 self.state_label.text = "loading"
                 message = str(sum(self.data) / len(self.data))#傳送平均lux
+                message = "{} {} {}".format(sum(self.data) / len(self.data) , self.Acid1, self.Acid2)#傳送平均lux,Acid1,Acid2
                 self.sned_message(message)
                 self.state_label.text = "send : " + message
 
@@ -71,21 +78,24 @@ class Sensor_Light_Layout(BoxLayout):
     def sned_message(self,message):
         self.internet.sned_message(message)
 
-    def close(self,instance=None):
+    def close(self,instance=None,change = ""):
         try:
             self.internet.close_connet()
-            chat.screen_manager.current = "loing"
             Clock.unschedule(self.get_illumination)#關閉光線讀取
             light.disable()
+            if change != "don't change page":
+                chat.screen_manager.current = "loing"
         except :
             pass
 
-
+    def setting(self,instance):
+        self.close(change = "don't change page")
+        chat.screen_manager.current = "Setting"
     def init_connect(self,ip = '0.tcp.ngrok.io',port = 19446):
         try:
             self.internet.init_connect(ip,port)
             threading.Thread(target=self.listen_sever).start()
-            self.state_label.text = "stater connect"
+            self.state_label.text = "stater connect\nAcid1 : {}\nAcid1 : {}".format(self.Acid1,self.Acid2)
             light.enable()  # 開啟光線讀取
         except:
             pass
@@ -97,8 +107,8 @@ class Sensor_Light_Layout(BoxLayout):
             print(self.illumination)
 
             if len(self.data) >= 5:#儲存最新的5筆資料
-                del self.data[0]
-                self.data.append(self.illumination)
+                del self.data[0] #刪除最舊的一筆
+                self.data.append(self.illumination)#加入最新資料
             else:
                 self.data.append(self.illumination)
 
@@ -253,9 +263,21 @@ class Loing_Layout(BoxLayout):
     def start_chat(self,instance):
         try:
             if self.username.text == "Light":
-                chat.light_page.init_connect(self.ip.text,int(self.port.text))
-                chat.light_page.sned_message("s_l")
-                chat.screen_manager.current = self.username.text
+                try:
+                    with open("acid.cfg", "r") as f:
+                        c = f.readlines()
+                        a = float(c[0].rstrip("\n"))
+                        b = float(c[1])
+                        chat.light_page.init_connect(self.ip.text, int(self.port.text))
+                        chat.light_page.sned_message("s_l")
+                        chat.light_page.Acid1 = a
+                        chat.light_page.Acid2 = b
+                        chat.screen_manager.current = self.username.text
+                except:
+                    chat.screen_manager.current = "Setting"
+
+
+
             else:
                 chat.acceleration_page.init_connect(self.ip.text,int(self.port.text))
                 chat.acceleration_page.sned_message("s_a")
@@ -268,6 +290,55 @@ class Loing_Layout(BoxLayout):
             print("connect error : ",e)
             pass
 
+class Setting_Layout(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        self.padding = '10dp'
+        self.spacing = '30dp'
+
+        self.Grid = GridLayout(cols=2)
+        self.Grid.spacing = '5dp'
+
+        self.Grid.add_widget(Label(text='Acid_1:',size_hint = (0.4,1)))  # widget #1, top left
+        self.Acid_1 = TextInput(text="10", multiline=False,size_hint = (0.6,1),input_type = "number")  # defining self.ip...
+        self.Grid.add_widget(self.Acid_1) # widget #2, top right
+
+        self.Grid.add_widget(Label(text='Acid_2:',size_hint = (0.4,1)))
+        self.Acid_2 = TextInput(text="10", multiline=False,size_hint = (0.6,1),input_type = "number")
+        self.Grid.add_widget(self.Acid_2)
+
+        self.add_widget(self.Grid)
+
+        self.set = Button(text="Start",size_hint = (1,0.2))
+        self.set.bind(on_press=self.set_acid)
+        self.add_widget(self.set)
+
+        self.Error_box = Popup(size_hint = (None,None),size = (400,400))
+        self.Error_layout = BoxLayout(orientation = "vertical")
+        self.Error_box_label = Label(text = "",size_hint=(1,0.8))
+        self.Error_layout.add_widget(self.Error_box_label)
+        self.Error_layout.add_widget(Button(text = "Close",on_press=self.Error_box.dismiss,size_hint=(1,0.2)))
+        self.Error_box.content = self.Error_layout
+
+        self.Acid1 = 0
+        self.Acid2 = 0
+    def set_acid(self,instance):
+        try:
+            with open("acid.cfg", "w") as f:
+                self.Acid1 = float(self.Acid_1.text)
+                self.Acid2 = float(self.Acid_2.text)
+                f.write(str(self.Acid1) + "\n" + str(self.Acid2))
+            chat.light_page.Acid1 = self.Acid1
+            chat.light_page.Acid2 = self.Acid2
+            chat.light_page.init_connect(chat.loing_page.ip.text, int(chat.loing_page.port.text))
+            chat.light_page.sned_message("s_l")
+            chat.screen_manager.current = "Light"
+        except ValueError:
+            self.Error_box.title = "Set erroe"
+            self.Error_box_label.text = "Plase enter number,try again"
+            self.Error_box_label.text_size = (self.Error_box_label.width,None)
+            self.Error_box.open()
 
 class Chat(App):
     def build(self):
@@ -287,6 +358,11 @@ class Chat(App):
         self.acceleration_page = Sensor_Accelerometer_Layout()
         screen = Screen(name='Acceleration')
         screen.add_widget(self.acceleration_page)
+        self.screen_manager.add_widget(screen)
+
+        self.setting_page = Setting_Layout()
+        screen = Screen(name='Setting')
+        screen.add_widget(self.setting_page)
         self.screen_manager.add_widget(screen)
 
         Window.bind(on_request_close=self.on_request_close)
